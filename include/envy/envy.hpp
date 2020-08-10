a65523170483c8720d5456652cd4d8241d4042f2
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-08-09 22:13:39 -0500

    Minor updates to support C++11 and better SFINAE

--- include/envy/envy.hpp
+++ include/envy/envy.hpp
@@ -1,40 +1,20 @@
 
 #pragma once
 #include <cstdlib>
+#include <envy/json.hpp>
+#include <envy/visit_struct/visit_struct.hpp>
 #include <iostream>
 #include <map>
 #include <sstream>
 #include <string>
 #include <type_traits>
 #include <vector>
-#include <envy/json.hpp>
-#include <envy/visit_struct/visit_struct.hpp>
 
 #define ENVY_CONFIG VISITABLE_STRUCT
 
 namespace envy {
 
 namespace details { // namespace for helper methods
-
-template <typename... Ts> struct is_container_helper {};
-
-template <typename T, typename _ = void>
-struct is_container : std::false_type {};
-
-template <> struct is_container<std::string> : std::false_type {};
-
-template <typename T>
-struct is_container<
-    T,
-    std::conditional_t<false,
-                       is_container_helper<typename T::value_type,
-                                           decltype(std::declval<T>().begin()),
-                                           decltype(std::declval<T>().end()),
-                                           decltype(std::declval<T>().size())>,
-                       void>> : std::true_type {};
-
-template <typename T>
-static constexpr bool is_container_v = is_container<T>::value;
 
 template <typename Test, template <typename...> class Ref>
 struct is_specialization : std::false_type {};
@@ -44,7 +24,11 @@ struct is_specialization<Ref<Args...>, Ref> : std::true_type {};
 
 // Any field that is not a container type
 template <typename T>
-inline typename std::enable_if<!details::is_container<T>::value, T>::type
+inline typename std::enable_if<
+    !(details::is_specialization<T, std::vector>::value ||
+      details::is_specialization<T, std::map>::value ||
+      details::is_specialization<T, std::unordered_map>::value),
+    T>::type
 convert_to(const std::string &str) {
   std::istringstream ss(str);
   T num;
@@ -54,11 +38,10 @@ convert_to(const std::string &str) {
 
 // std::vector
 template <typename T>
-inline typename std::enable_if<
-    details::is_container<T>::value &&
-        details::is_specialization<T, std::vector>::value,
-    T>::type
-convert_to(const std::string &str) {
+inline
+    typename std::enable_if<details::is_specialization<T, std::vector>::value,
+                            T>::type
+    convert_to(const std::string &str) {
   T result;
   std::stringstream ss(str);
 
@@ -73,11 +56,8 @@ convert_to(const std::string &str) {
 // std::map or std::unordered_map
 template <typename T>
 inline typename std::enable_if<
-    details::is_container<T>::value &&
-        (
-          details::is_specialization<T, std::map>::value ||
-          details::is_specialization<T, std::unordered_map>::value
-        ),
+    (details::is_specialization<T, std::map>::value ||
+     details::is_specialization<T, std::unordered_map>::value),
     T>::type
 convert_to(const std::string &str) {
   T result;
