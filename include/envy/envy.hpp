b6883e13c1857abe5e4547a3460f2bb03451470f
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-08-09 20:42:19 -0500

    Added support for prefix to env vars

--- include/envy/envy.hpp
+++ include/envy/envy.hpp
@@ -18,24 +18,27 @@ template <typename T> T convert_to(const std::string &str) {
 }
 
 struct get_env {
+  const std::string prefix;
+
   template <typename T> void operator()(const char *name, T &value) {
     std::string field_name = name;
     std::transform(field_name.begin(), field_name.end(), field_name.begin(),
                    ::toupper);
-    char * env = std::getenv(field_name.c_str());
+    char * env = std::getenv((prefix + field_name).c_str());
     if (env) {
       value = convert_to<T>(env);
     }
   }
 };
 
-template <typename T> void visit(T &config) {
-  visit_struct::for_each(config, get_env{});
+template <typename T> void visit(T &config, const std::string & prefix) {
+  get_env get_env_visitor {.prefix = prefix};
+  visit_struct::for_each(config, get_env_visitor);
 }
 
-template <typename T> T get_env() {
+template <typename T> T get_env(const std::string & prefix = "") {
   T result;
-  visit(result);
+  visit(result, prefix);
   return result;
 }
 
