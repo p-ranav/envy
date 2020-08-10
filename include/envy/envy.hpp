676cc212e1f441f5598f1743c0c9e3a04bc5932a
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-08-09 21:14:14 -0500

    Added support for parsing JSON into std::map or std::unordered_map

--- include/envy/envy.hpp
+++ include/envy/envy.hpp
@@ -2,11 +2,13 @@
 #pragma once
 #include <cstdlib>
 #include <iostream>
+#include <map>
 #include <sstream>
 #include <string>
 #include <type_traits>
 #include <vector>
-#include <visit_struct/visit_struct.hpp>
+#include <envy/json.hpp>
+#include <envy/visit_struct/visit_struct.hpp>
 
 #define ENVY_CONFIG VISITABLE_STRUCT
 
@@ -34,9 +36,16 @@ struct is_container<
 template <typename T>
 static constexpr bool is_container_v = is_container<T>::value;
 
-}
+template <typename Test, template <typename...> class Ref>
+struct is_specialization : std::false_type {};
 
-template <typename T> 
+template <template <typename...> class Ref, typename... Args>
+struct is_specialization<Ref<Args...>, Ref> : std::true_type {};
+
+} // namespace details
+
+// Any field that is not a container type
+template <typename T>
 inline typename std::enable_if<!details::is_container<T>::value, T>::type
 convert_to(const std::string &str) {
   std::istringstream ss(str);
@@ -45,17 +54,43 @@ convert_to(const std::string &str) {
   return num;
 }
 
+// std::vector
 template <typename T>
-inline typename std::enable_if<details::is_container<T>::value, T>::type
-convert_to(const std::string & str) {
+inline typename std::enable_if<
+    details::is_container<T>::value &&
+        details::is_specialization<T, std::vector>::value,
+    T>::type
+convert_to(const std::string &str) {
   T result;
   std::stringstream ss(str);
 
   for (typename T::value_type i; ss >> i;) {
-    result.push_back(i);    
-      if (ss.peek() == ',')
-        ss.ignore();
-    }
+    result.push_back(i);
+    if (ss.peek() == ',')
+      ss.ignore();
+  }
+  return result;
+}
+
+// std::map or std::unordered_map
+template <typename T>
+inline typename std::enable_if<
+    details::is_container<T>::value &&
+        (
+          details::is_specialization<T, std::map>::value ||
+          details::is_specialization<T, std::unordered_map>::value
+        ),
+    T>::type
+convert_to(const std::string &str) {
+  T result;
+  nlohmann::json j;
+
+  std::stringstream ss(str);
+  ss >> j;
+
+  for (nlohmann::json::iterator it = j.begin(); it != j.end(); ++it) {
+    result.insert(std::make_pair(it.key(), it.value()));
+  }
   return result;
 }
 
@@ -66,33 +101,31 @@ struct get_env {
     std::string field_name = name;
     std::transform(field_name.begin(), field_name.end(), field_name.begin(),
                    ::toupper);
-    char * env = std::getenv((prefix + field_name).c_str());
+    char *env = std::getenv((prefix + field_name).c_str());
     if (env) {
       value = convert_to<T>(env);
     }
   }
 };
 
-template <typename T> void visit(T &config, const std::string & prefix) {
-  get_env get_env_visitor {.prefix = prefix};
+template <typename T> void visit(T &config, const std::string &prefix) {
+  get_env get_env_visitor{.prefix = prefix};
   visit_struct::for_each(config, get_env_visitor);
 }
 
-template <typename T> T get_env(const std::string & prefix = "") {
+template <typename T> T get_env(const std::string &prefix = "") {
   T result;
   visit(result, prefix);
   return result;
 }
 
 struct debug_printer {
-  template <typename T>
-  void operator()(const char * name, const T & value) {
+  template <typename T> void operator()(const char *name, const T &value) {
     std::cerr << name << ": " << value << std::endl;
   }
 };
 
-template <typename T>
-void print(const T & config) {
+template <typename T> void print(const T &config) {
   visit_struct::for_each(config, debug_printer{});
 }
 
